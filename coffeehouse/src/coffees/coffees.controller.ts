5a648804d38f736eca3b0262c0151611a891dc55
Anush Markosyan <Anush_Markosyan@epam.com>
2021-12-14 17:33:09 +0400

    added coffees module and connected with app module

--- coffeehouse/src/coffees/coffees.controller.ts
+++ coffeehouse/src/coffees/coffees.controller.ts
@@ -1,20 +1,48 @@
-import {Controller, Get, Post, Param, Body} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get('getAll')
-  getAll() {
-    return 'This will return all coffees))))';
+  getAll(@Query() querySting) {
+     //const { limit, offset } = querySting;
+     //return `This will return all coffees. Limit: ${limit}, Offset: ${offset}))))`;
+    return this.coffeesService.getAll();
   }
 
   @Get(':id')
- // @HttpCode(HttpStatus.GONE) //for 410 status code
+  // @HttpCode(HttpStatus.GONE) //for 410 status code
   getOne(@Param('id') id: string) {
-    return `This is route with id #${id}.`;
+    // return `This is route with id #${id}.`;
+    return this.coffeesService.getOne(id);
   }
 
   @Post()
   createOne(@Body() body) {
-    return body;
+    //return body;
+    return this.coffeesService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    //return `This is for updating route with id #${id}.`;
+    return this.coffeesService.update(id, body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    // return `This is for deleting route with id #${id}.`;
+    return this.coffeesService.delete(id);
   }
 }
