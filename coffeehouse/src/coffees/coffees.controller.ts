30b54c69bd9deb0ceb2316225d1ed44bc45de0af
Anush Markosyan <Anush_Markosyan@epam.com>
2021-12-20 12:20:48 +0400

    added pagination for queries

--- coffeehouse/src/coffees/coffees.controller.ts
+++ coffeehouse/src/coffees/coffees.controller.ts
@@ -11,14 +11,15 @@ import {
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from "./dto/create-coffee.dto";
 import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
+import {PaginationQueryDto} from "../common/dto/pagination-query.dto";
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get('getAll')
-  getAll(@Query() querySting) {
-    return this.coffeesService.getAll();
+  getAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.getAll(paginationQuery);
   }
 
   @Get(':id')
--- coffeehouse/src/coffees/coffees.service.ts
+++ coffeehouse/src/coffees/coffees.service.ts
@@ -11,6 +11,7 @@ import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 
 @Injectable()
 export class CoffeesService {
@@ -20,9 +21,12 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
   ) {}
-  async getAll() {
+  async getAll(paginationQuery: PaginationQueryDto) {
+    const { limit, offset } = paginationQuery;
     return await this.coffeeRepository.find({
       relations: ['flavors'],
+      skip: offset,
+      take: limit,
     });
   }
 
